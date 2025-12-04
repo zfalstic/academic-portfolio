031c30b3a948b6eef46b3619c3678bf5ebec0c7c
Dawson Zhang <dawsonzhang@utexas.edu>
2025-12-04 09:16:45 -0600

    Added ECE 306H Program 5

--- F25/ECE_306H/assignments/program-05/program05.asm
+++ F25/ECE_306H/assignments/program-05/program05.asm
@@ -1,6 +1,6 @@
 ; Program5.asm
-; Name(s):
-; UTEid(s): 
+; Name(s): Nathan Aaker, Dawson Zhang
+; UTEid(s): nxa65, ddz249
 ; Continuously reads from x3500 making sure its not reading duplicate
 ; symbols. Processes the symbol based on the program description
 ; of mRNA processing.
@@ -13,7 +13,7 @@ STI R0, KBINTVec
 ; enable keyboard interrupts
 ; KBSR[14] <- 1 ==== M[xFE00] = x4000
 LDI R0, KBSR
-LD R1, KBIEN
+LD R1, KBINTEN
 NOT R0, R0
 NOT R1, R1
 AND R0, R0, R1
@@ -25,22 +25,81 @@ Loop
     LDI R0, GLOB
     BRz Loop             
 ; Process it
-
+    TRAP x21
+    AND R0, R0, #0
+    STI R0, GLOB
+    LD R1, CurrentState
+    LDI R0, GLOB2
+    ADD R1, R1, R0
+    LDR R2, R1, #0 ;Has next state
+    BRz StopDetected
+    LDR R0, R2, #0 ;Output for next state
+    BRz NoOutput
+    TRAP x21
+NoOutput
+    ST R2, CurrentState
+    BR Loop
+StopDetected
 ; Repeat until Stop Codon detected
     HALT
 KBINTVec  .FILL x0180
 KBSR   .FILL xFE00
-KBIEN   .FILL x4000
 KBISR  .FILL x2500
 KBINTEN  .FILL x4000
 GLOB   .FILL x3500
+GLOB2   .FILL x3501
 
+CurrentState    .FILL Start
+Start   .FILL x00
+        .FILL StartA
+        .FILL Start
+        .FILL Start
+        .FILL Start
+StartA  .FILL x00
+        .FILL StartA
+        .FILL Start
+        .FILL StartAU
+        .FILL Start
+StartAU .FILL x00
+        .FILL Start
+        .FILL Start
+        .FILL Start
+        .FILL StartAUG
+StartAUG
+        .FILL x7c
+        .FILL RepeatRNA
+        .FILL RepeatRNA
+        .FILL StopU
+        .FILL RepeatRNA
+RepeatRNA
+        .FILL x00
+        .FILL RepeatRNA
+        .FILL RepeatRNA
+        .FILL StopU
+        .FILL RepeatRNA
+StopU   .FILL x00
+        .FILL StopUA
+        .FILL RepeatRNA
+        .FILL StopU
+        .FILL StopUG
+StopUA  .FILL x00
+        .FILL #0
+        .FILL RepeatRNA
+        .FILL StopU
+        .FILL #0
+StopUG  .FILL x00
+        .FILL #0
+        .FILL RepeatRNA
+        .FILL StopU
+        .FILL RepeatRNA
 	.END
 
 ; Interrupt Service Routine
 ; Keyboard ISR runs when a key is struck
 ; Checks for a valid RNA symbol and places it at x3500
         .ORIG x2500
+        ST R0, ISR_R0
+        ST R1, ISR_R1
         
         LDI R0, KBDR
         LD R1, KBDRMask
@@ -48,32 +107,54 @@ GLOB   .FILL x3500
         
         LD R1, -A
         ADD R1, R0, R1
-        BRz isValid
+        BRnp NotA
+        AND R1, R1 #0
+        ADD R1, R1, #1
+        STI R1, IGLOB2
+        STI R0, IGLOB
+        BRnzp Return
+NotA
         
         LD R1, -C
         ADD R1, R0, R1
-        BRz isValid
+        BRnp NotC
+        AND R1, R1 #0
+        ADD R1, R1, #2
+        STI R1, IGLOB2
+        STI R0, IGLOB
+        BRnzp Return
+NotC
         
         LD R1, -U
         ADD R1, R0, R1
-        BRz isValid
+        BRnp NotU
+        AND R1, R1 #0
+        ADD R1, R1, #3
+        STI R1, IGLOB2
+        STI R0, IGLOB
+        BRnzp Return
+NotU
         
         LD R1, -G
         ADD R1, R0, R1
-        BRz isValid
-        
-        BRnzp return
-        
-isValid
+        BRnp Return
+        AND R1, R1 #0
+        ADD R1, R1, #4
+        STI R1, IGLOB2
         STI R0, IGLOB
+Return
+
+        LD R0, ISR_R0
+        LD R1, ISR_R1
         
-return
- 
         RTI
         
+ISR_R0  .BLKW #1
+ISR_R1  .BLKW #1
 KBDR  .FILL xFE02
 KBDRMask .FILL x00FF
 IGLOB  .FILL x3500
+IGLOB2  .FILL x3501
 -A  .FILL #-65
 -C  .FILL #-67
 -U  .FILL #-85
